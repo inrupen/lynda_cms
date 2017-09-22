7f7f53c5c115e09e3ef0d7882856e3c63feef091
inrupen <inrupen@gmail.com>
2017-09-21 23:47:19 -0500

    Added column to the table created

--- db/migrate/20170922041642_create_users.rb
+++ db/migrate/20170922041642_create_users.rb
@@ -1,8 +1,20 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
-  def change
+  def up
     create_table :users do |t|
 
-      t.timestamps
+      t.column "first_name", :string,  :limit => 25           #can also be -- t.string "first_name", :limit => 25
+      t.string "last_name", :limit => 50
+      t.string "email", :default =>'', :null => false
+      t.string "password", :limit => 50
+
+      t.timestamps                #this is used insted of the below 2 datetime - Ruby Default
+      #t.datetime "created_at"
+      #t.datatime "updated_at"
     end
   end
+
+  def down
+
+  end
+
 end
